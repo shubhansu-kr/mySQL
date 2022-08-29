e2a780fca7ae1c7878a55ee553a222b0997ac355
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-29 11:46:49 +0530

    Practice

---
+++ 08_Practice.sql
@@ -0,0 +1,9 @@
+-- Select data from table: 
+/* 
+1. All coulumn All row
+2. All column specific row
+3. specific colums all row 
+4. specific column specific row 
+*/
+
+-- http://172.17.14.100/isqlplus
---
+++ 09_Alter.sql
@@ -0,0 +1,10 @@
+CREATE TABLE mobile (
+    num NUMERIC(10)
+);
+
+-- Add name column to the existing column 
+-- ALTER TABLE mobile ADD (mName VARCHAR(26), price NUMERIC(10));
+-- Table altered 
+
+-- DESC mobile;
+
